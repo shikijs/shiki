5c8146fa77b7bf462dd16ad18a9877c06c70c772
Pine Wu <octref@gmail.com>
2019-01-01 21:09:36 -0800

    leftover

--- packages/shiki/src/index.ts
+++ packages/shiki/src/index.ts
@@ -3,15 +3,3 @@ export { getTheme, loadTheme } from 'shiki-themes'
 export { getHighlighter } from './highlighter';
 export { renderToHtml } from './renderer';
 export { IThemedToken } from './themedTokenizer';
-
-const shiki = require('shiki')
-
-shiki.getHighlighter({
-  theme: 'nord'
-}).then(highlighter => {
-  console.log(highlighter.codeToHtml(`console.log('shiki');`, 'js'))
-})
-
-// <pre class="shiki" style="background-color: #2e3440"><code>
-//   <!-- Highlighted Code -->
-// </code></pre
