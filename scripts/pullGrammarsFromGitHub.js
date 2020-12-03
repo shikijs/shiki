d61fb5dabea9230684e928f8d060315ac3a62add
Yu Zhang <583181285@qq.com>
2020-12-04 01:30:39 +0800

    Add ssh-config syntax highlight

--- scripts/pullGrammarsFromGitHub.js
+++ scripts/pullGrammarsFromGitHub.js
@@ -65,7 +65,8 @@ const languageSources = [
   'https://github.com/wenyan-lang/highlight/blob/master/wenyan.tmLanguage.json',
   'https://github.com/elixir-editors/elixir-tmbundle/blob/master/Syntaxes/Elixir.tmLanguage',
   'https://github.com/pgourlain/vscode_erlang/blob/master/syntaxes/erlang.tmLanguage',
-  'https://github.com/textmate/applescript.tmbundle/blob/master/Syntaxes/AppleScript.tmLanguage'
+  'https://github.com/textmate/applescript.tmbundle/blob/master/Syntaxes/AppleScript.tmLanguage',
+  'https://github.com/textmate/ssh-config.tmbundle/blob/master/Syntaxes/SSH-Config.tmLanguage'
 ]
 
 async function go() {
