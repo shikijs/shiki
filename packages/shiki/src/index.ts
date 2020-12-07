e2012714114be082467718ca5d6f0204d89f0bd5
Anthony Fu <anthonyfu117@hotmail.com>
2020-12-07 22:32:57 +0800

    chore: export more types

--- packages/shiki/src/index.ts
+++ packages/shiki/src/index.ts
@@ -1,6 +1,7 @@
-export { themes as BUNDLED_THEMES } from './themes'
-export { languages as BUNDLED_LANGUAGES } from './languages'
+export { themes as BUNDLED_THEMES, Theme } from './themes'
+export { languages as BUNDLED_LANGUAGES, Lang } from './languages'
 
 export { getHighlighter, Highlighter } from './highlighter'
 export { renderToHtml } from './renderer'
 export { IThemedToken } from './themedTokenizer'
+export { ILanguageRegistration, IShikiTheme, IThemeRegistration } from './types'
