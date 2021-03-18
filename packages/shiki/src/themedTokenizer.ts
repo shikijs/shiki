48637fced3d3e07109ac930a9833d81f8989a9d5
Justice Almanzar <superdash993@gmail.com>
2021-03-18 04:21:19 -0400

    support font styles

--- packages/shiki/src/themedTokenizer.ts
+++ packages/shiki/src/themedTokenizer.ts
@@ -133,6 +133,22 @@ export function tokenizeWithTheme(
       }
       let metadata = result.tokens[2 * j + 1]
       let foreground = StackElementMetadata.getForeground(metadata)
+      let fontStyle = StackElementMetadata.getFontStyle(metadata)
+      let fontStyleText
+      switch (fontStyle) {
+        case 1: {
+          fontStyleText = 'italic'
+          break
+        }
+        case 2: {
+          fontStyleText = 'bold'
+          break
+        }
+        case 3: {
+          fontStyleText = 'underline'
+          break
+        }
+      }
       let foregroundColor = colorMap[foreground]
       let fontStyle: FontStyle = StackElementMetadata.getFontStyle(metadata)
 
@@ -156,12 +172,16 @@ export function tokenizeWithTheme(
         }
       }
 
-      actual.push({
+      let token: IThemedToken = {
         content: line.substring(startIndex, nextStartIndex),
         color: foregroundColor,
         fontStyle,
         explanation: explanation
-      })
+      }
+      if (fontStyleText) {
+        token.style = fontStyleText
+      }
+      actual.push(token)
     }
     final.push(actual)
     actual = []
