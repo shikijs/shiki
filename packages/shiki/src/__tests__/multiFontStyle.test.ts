c4c9ad4f32ddd1ec14ad07464e7e466df7e8c1a3
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2021-09-18 18:30:03 -0600

    refactor: Make tests take less than half as long to run

--- packages/shiki/src/__tests__/multiFontStyle.test.ts
+++ packages/shiki/src/__tests__/multiFontStyle.test.ts
@@ -2,7 +2,8 @@ import { getHighlighter } from '../index'
 
 test('Handle multiple font styles', async () => {
   const highlighter = await getHighlighter({
-    theme: 'material-default'
+    theme: 'material-default',
+    langs: ['md']
   })
   const out = highlighter.codeToHtml(`***bold italic***`, 'md')
   expect(out).toMatchSnapshot()
--- packages/shiki/src/__tests__/simple.test.ts
+++ packages/shiki/src/__tests__/simple.test.ts
@@ -2,7 +2,8 @@ import { getHighlighter } from '../index'
 
 test('Nord highlighter highlights simple JavaScript', async () => {
   const highlighter = await getHighlighter({
-    theme: 'nord'
+    theme: 'nord',
+    langs: ['js']
   })
   const out = highlighter.codeToHtml(`console.log('shiki');`, 'js')
   expect(out).toMatchSnapshot()
