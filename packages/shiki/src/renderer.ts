dba3ce6280ce1571ab9f0ccf790c8663a5731caa
MO <fengzilong1992@gmail.com>
2021-10-09 21:46:48 +0800

    feat: support custom elements for html renderer

--- packages/shiki/src/renderer.ts
+++ packages/shiki/src/renderer.ts
@@ -1,45 +1,123 @@
 import { FontStyle } from './stackElementMetadata'
 import { IThemedToken } from './themedTokenizer'
 
+export interface ElementProps {
+  children: string
+  [key: string]: unknown
+}
+
+export interface PreElementProps extends ElementProps {
+  className: string
+  style: string
+}
+
+export interface CodeElementProps extends ElementProps {}
+
+export interface LineElementProps extends ElementProps {
+  className: string
+  lines: IThemedToken[][]
+  line: IThemedToken[]
+  index: number
+}
+
+export interface TokenElementProps extends ElementProps {
+  style: string
+  tokens: IThemedToken[]
+  token: IThemedToken
+  index: number
+}
+
+export interface ElementsOptions {
+  pre?: (props: PreElementProps) => string
+  code?: (props: CodeElementProps) => string
+  line?: (props: LineElementProps) => string
+  token?: (props: TokenElementProps) => string
+}
+
 export interface HtmlRendererOptions {
   langId?: string
   fg?: string
   bg?: string
+  elements?: ElementsOptions
+}
+
+const defaultElements: ElementsOptions = {
+  pre({ className, style, children }) {
+    return `<pre class="${className}" style="${style}">${children}</pre>`
+  },
+
+  code({ children }) {
+    return `<code>${children}</code>`
+  },
+
+  line({ children }) {
+    return `<span class="line">${children}</span>`
+  },
+
+  token({ style, children }) {
+    return `<span style="${style}">${children}</span>`
+  }
 }
 
 export function renderToHtml(lines: IThemedToken[][], options: HtmlRendererOptions = {}) {
   const bg = options.bg || '#fff'
+  const userElements = options.elements || {}
 
-  let html = ''
+  function h(type: string = '', props = {}, children: string[]): string {
+    const element = userElements[type] || defaultElements[type]
+    if (element) {
+      children = children.filter(Boolean)
 
-  html += `<pre class="shiki" style="background-color: ${bg}">`
-  if (options.langId) {
-    html += `<div class="language-id">${options.langId}</div>`
+      return element({
+        ...props,
+        children: type === 'code' ? children.join('\n') : children.join('')
+      })
+    }
+
+    return ''
   }
-  html += `<code>`
 
-  lines.forEach((l: IThemedToken[]) => {
-    html += `<span class="line">`
+  return h('pre', { className: 'shiki', style: `background-color: ${bg}` }, [
+    options.langId ? `<div class="language-id">${options.langId}</div>` : '',
+    h(
+      'code',
+      {},
+      lines.map((line, index) => {
+        return h(
+          'line',
+          {
+            className: 'line',
+            lines,
+            line,
+            index
+          },
+          line.map((token, index) => {
+            const cssDeclarations = [`color: ${token.color || options.fg}`]
+            if (token.fontStyle & FontStyle.Italic) {
+              cssDeclarations.push('font-style: italic')
+            }
+            if (token.fontStyle & FontStyle.Bold) {
+              cssDeclarations.push('font-weight: bold')
+            }
+            if (token.fontStyle & FontStyle.Underline) {
+              cssDeclarations.push('text-decoration: underline')
+            }
 
-    l.forEach(token => {
-      const cssDeclarations = [`color: ${token.color || options.fg}`]
-      if (token.fontStyle & FontStyle.Italic) {
-        cssDeclarations.push('font-style: italic')
-      }
-      if (token.fontStyle & FontStyle.Bold) {
-        cssDeclarations.push('font-weight: bold')
-      }
-      if (token.fontStyle & FontStyle.Underline) {
-        cssDeclarations.push('text-decoration: underline')
-      }
-      html += `<span style="${cssDeclarations.join('; ')}">${escapeHtml(token.content)}</span>`
-    })
-    html += `</span>\n`
-  })
-  html = html.replace(/\n*$/, '') // Get rid of final new lines
-  html += `</code></pre>`
-
-  return html
+            return h(
+              'token',
+              {
+                style: cssDeclarations.join('; '),
+                tokens: line,
+                token,
+                index
+              },
+              [escapeHtml(token.content)]
+            )
+          })
+        )
+      })
+    )
+  ])
 }
 
 const htmlEscapes = {
