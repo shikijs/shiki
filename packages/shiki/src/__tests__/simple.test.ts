de2ca65af4397b3d9fc814f5cd49bf37c4158f9c
Steve Bauman <6421846+stevebauman@users.noreply.github.com>
2021-11-17 16:25:47 -0500

    Add failing test

--- packages/shiki/src/__tests__/simple.test.ts
+++ packages/shiki/src/__tests__/simple.test.ts
@@ -8,3 +8,15 @@ test('Nord highlighter highlights simple JavaScript', async () => {
   const out = highlighter.codeToHtml(`console.log('shiki');`, 'js')
   expect(out).toMatchSnapshot()
 })
+
+test('Highlighter can load languages', async () => {
+  const highlighter = await getHighlighter({
+    theme: 'nord',
+    langs: ['js']
+  })
+
+  await highlighter.loadLanguage('php')
+  await highlighter.loadLanguage('css')
+
+  expect(highlighter.getLoadedLanguages()).toEqual(['javascript', 'js', 'php', 'css'])
+})
