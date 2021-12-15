8774c0b7f58bd4213305e7fd80aabd22b978a65b
John Reilly <johnny_reilly@hotmail.com>
2021-12-15 18:22:53 +0000

    feat: add bicep support (#239)

--- packages/shiki/src/languages.ts
+++ packages/shiki/src/languages.ts
@@ -14,6 +14,7 @@ export type Lang =
   | 'ballerina'
   | 'bat' | 'batch'
   | 'bibtex'
+  | 'bicep'
   | 'c'
   | 'clojure' | 'clj'
   | 'cobol'
@@ -208,6 +209,12 @@ export const languages: ILanguageRegistration[] = [
     id: 'bibtex',
     scopeName: 'text.bibtex',
     path: 'bibtex.tmLanguage.json'
+  },
+  {
+    id: 'bicep',
+    scopeName: 'source.bicep',
+    path: 'bicep.tmLanguage.json',
+    samplePath: 'bicep.sample'
   },
   {
     id: 'c',
