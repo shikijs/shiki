984e6274001dde1969fdd13d18a53a6e837d6f1b
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2022-09-08 16:42:43 -0700

    Update

--- packages/shiki/src/languages.ts
+++ packages/shiki/src/languages.ts
@@ -603,6 +603,7 @@ export const languages: ILanguageRegistration[] = [
     id: 'marko',
     scopeName: 'text.marko',
     path: 'marko.tmLanguage.json',
+    samplePath: 'marko.sample',
     embeddedLangs: ['css', 'less', 'scss', 'typescript']
   },
   {
