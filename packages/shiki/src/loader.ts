d0ab9a8093a98e72fd665697081ec80652d96aba
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2022-09-09 11:42:46 -0700

    Updates

--- packages/shiki/src/loader.ts
+++ packages/shiki/src/loader.ts
@@ -46,7 +46,7 @@ export async function getOniguruma(): Promise<IOnigLib> {
   if (!_onigurumaPromise) {
     let loader: Promise<void>
 
-    if (isBrowser) {
+    if (isBrowser || WASM) {
       if (typeof WASM === 'string') {
         loader = loadWASM({
           data: await fetch(_resolvePath('dist/onig.wasm')).then(r => r.arrayBuffer())
