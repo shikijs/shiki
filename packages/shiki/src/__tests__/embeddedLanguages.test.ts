4fd5da9fd0c8b2c55d50d89c5dc1342c678bf13a
muenzpraeger <mail@winkelmeyer.com>
2022-12-02 00:02:06 +0100

    feat: add test for embedded languages

---
+++ packages/shiki/src/__tests__/embeddedLanguages.test.ts
@@ -0,0 +1,11 @@
+import { getHighlighter } from '../index'
+
+test('Requesting html as language also loads the embedded languages like JavaScript or CSS', async () => {
+  const highlighter = await getHighlighter({
+    theme: 'nord',
+    langs: ['html']
+  })
+  const languages = highlighter.getLoadedLanguages()
+  console.log(languages)
+  expect(languages.sort()).toEqual(['html', 'css', 'javascript', 'js'].sort())
+})
