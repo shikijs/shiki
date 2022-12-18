b20bc46782bf8c11fa07474b903fd7015e89dd39
orta <git@orta.io>
2022-12-18 17:48:23 +0000

    Run all pre deploy update scripts

--- packages/shiki/src/languages.ts
+++ packages/shiki/src/languages.ts
@@ -619,7 +619,6 @@ export const languages: ILanguageRegistration[] = [
     id: 'marko',
     scopeName: 'text.marko',
     path: 'marko.tmLanguage.json',
-    samplePath: 'marko.sample',
     embeddedLangs: ['css', 'less', 'scss', 'typescript']
   },
   {
