2be4fd8c8485ea3eb5c6bc4597e2e7ebcd9db8bf
Kevin Batdorf <kbat82@gmail.com>
2023-05-06 20:46:32 -0400

    remove unrelated update

--- packages/shiki/src/languages.ts
+++ packages/shiki/src/languages.ts
@@ -687,7 +687,6 @@ export const languages: ILanguageRegistration[] = [
     id: 'marko',
     scopeName: 'text.marko',
     path: 'marko.tmLanguage.json',
-    samplePath: 'marko.sample',
     embeddedLangs: ['css', 'less', 'scss', 'javascript']
   },
   {
