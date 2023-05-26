5208ee53bfae9373f217730e0777e0e96626a757
lawvs <18554747+lawvs@users.noreply.github.com>
2023-05-27 04:10:48 +0800

    chore: revert name field

--- scripts/grammars/normalizeGrammarPaths.ts
+++ scripts/grammars/normalizeGrammarPaths.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { vscodeGrammarsToRemove, vscodeGrammarsToRename } from '../grammarSources'
 import chalk from 'chalk'
+import { parseJson } from '../util/parse'
 
 const GRAMMAR_FOLDER_PATH = path.join(__dirname, '../..', 'tmp/grammars')
 
@@ -51,8 +52,16 @@ for (let f of files) {
 function normalizeGrammarFile(f: string) {
   const fPath = path.resolve(GRAMMAR_FOLDER_PATH, f)
   const fNameWithoutSuffix = f.replace(/\.tmLanguage.json$/i, '')
+  const fName = fNameWithoutSuffix.toLowerCase()
 
   if (fs.existsSync(fPath)) {
+    const parsedContent = parseJson(fs.readFileSync(fPath, 'utf-8'))
+
+    if (parsedContent.name !== fName) {
+      parsedContent.name = fName
+      fs.writeFileSync(fPath, JSON.stringify(parsedContent, null, 2))
+      console.log(`${chalk.red('normalized')} ${f}'s \`name\` to ${chalk.yellow(fName)}`)
+    }
     if (fNameWithoutSuffix !== fNameWithoutSuffix.toLowerCase()) {
       const fNewPath = path.resolve(
         GRAMMAR_FOLDER_PATH,
