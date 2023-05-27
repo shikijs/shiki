86f148af702d6556c51aa715680ea602aaf2e9b1
Pine Wu <octref@gmail.com>
2023-05-27 11:51:36 -0600

    fix: fix unzipping issue with unzipper. fix #478

--- scripts/util/downloadFromMarketplace.ts
+++ scripts/util/downloadFromMarketplace.ts
@@ -1,7 +1,7 @@
 import { get } from './download'
 import fs from 'fs'
 import path from 'path'
-import unzipper from 'unzipper'
+import Admzip from 'adm-zip'
 import chalk from 'chalk'
 import kebab from 'lodash.kebabcase'
 
@@ -22,61 +22,58 @@ export async function downloadFromMarketplace(
   const zipPath = `tmp/${extId}.zip`
   fs.writeFileSync(zipPath, content)
 
-  fs.createReadStream(zipPath)
-    .pipe(unzipper.Extract({ path: `tmp/${extId}` }))
-    .on('close', () => {
-      try {
-        const packageJson = JSON.parse(
-          fs.readFileSync(`tmp/${extId}/extension/package.json`, 'utf-8')
-        )
-        for (let id of langsOrThemes) {
-          const contributesList =
-            type === 'grammar' ? packageJson.contributes.grammars : packageJson.contributes.themes
-          const fileSuffix = type === 'grammar' ? '.tmLanguage.json' : '.json'
+  const zip = new Admzip(zipPath)
+  zip.extractAllTo(`tmp/${extId}`)
 
-          const itemPath =
-            type === 'theme'
-              ? contributesList.find(c => c.label === id).path
-              : contributesList.find(c => {
-                  /**
-                   * Certain languages, for example `mermaid`, registers two languages for the same identifier
-                   *
-                   * {
-                   *   "grammars": [
-                   *     {
-                   *       "language": "mermaid",
-                   *       "scopeName": "markdown.mermaid.codeblock",
-                   *       "path": "./out/mermaid.tmLanguage-markdown.json"
-                   *     },
-                   *     {
-                   *       "language": "mermaid",
-                   *       "scopeName": "source.mermaid",
-                   *       "path": "./out/mermaid.tmLanguage.json"
-                   *     }
-                   *   ]
-                   * }
-                   *
-                   * In such case, look for the one with scopeName beginning with `source` or `text` as the canonical one
-                   */
-                  return (
-                    c.language === id &&
-                    (c.scopeName.startsWith('source') || c.scopeName.startsWith('text'))
-                  )
-                }).path
+  try {
+    const packageJson = JSON.parse(fs.readFileSync(`tmp/${extId}/extension/package.json`, 'utf-8'))
+    for (let id of langsOrThemes) {
+      const contributesList =
+        type === 'grammar' ? packageJson.contributes.grammars : packageJson.contributes.themes
+      const fileSuffix = type === 'grammar' ? '.tmLanguage.json' : '.json'
 
-          fs.copyFileSync(
-            path.resolve(`tmp/${extId}/extension`, itemPath),
-            `tmp/${type}s/${kebab(id.toLowerCase())}${fileSuffix}`
-          )
+      const itemPath =
+        type === 'theme'
+          ? contributesList.find(c => c.label === id).path
+          : contributesList.find(c => {
+              /**
+               * Certain languages, for example `mermaid`, registers two languages for the same identifier
+               *
+               * {
+               *   "grammars": [
+               *     {
+               *       "language": "mermaid",
+               *       "scopeName": "markdown.mermaid.codeblock",
+               *       "path": "./out/mermaid.tmLanguage-markdown.json"
+               *     },
+               *     {
+               *       "language": "mermaid",
+               *       "scopeName": "source.mermaid",
+               *       "path": "./out/mermaid.tmLanguage.json"
+               *     }
+               *   ]
+               * }
+               *
+               * In such case, look for the one with scopeName beginning with `source` or `text` as the canonical one
+               */
+              return (
+                c.language === id &&
+                (c.scopeName.startsWith('source') || c.scopeName.startsWith('text'))
+              )
+            }).path
 
-          console.log(
-            `${chalk.red('extracted')} ${type}: ${chalk.blue(id)} from ${chalk.yellow(extFullId)}`
-          )
-        }
-      } catch (err) {
-        throw Error(`Error extracting from extension ${extFullId}`)
-      }
-    })
+      fs.copyFileSync(
+        path.resolve(`tmp/${extId}/extension`, itemPath),
+        `tmp/${type}s/${kebab(id.toLowerCase())}${fileSuffix}`
+      )
+
+      console.log(
+        `${chalk.red('extracted')} ${type}: ${chalk.blue(id)} from ${chalk.yellow(extFullId)}`
+      )
+    }
+  } catch (err) {
+    throw Error(`Error extracting from extension ${extFullId}`)
+  }
 }
 
 function getMarketplaceLink(publisherDotExtId: string) {
