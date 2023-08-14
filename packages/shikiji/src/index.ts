dfc1d2546e2172c5387d20f8c25d603378db65d4
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-14 18:30:53 +0200

    chore: update import path

--- packages/shikiji/src/index.ts
+++ packages/shikiji/src/index.ts
@@ -1,12 +1,12 @@
 import type { BuiltinLanguages, BuiltinThemes, HighlighterGeneric } from './types'
 import { createSingletonShorthands, createdBundledHighlighter } from './core'
-import { bundledLanguages } from './assets/langs'
+import { bundledLanguages } from './langs'
 import { bundledThemes } from './themes'
 import { getWasmInlined } from './wasm'
 
 export * from './core'
 export * from './themes'
-export * from './assets/langs'
+export * from './langs'
 export * from './wasm'
 
 export type Highlighter = HighlighterGeneric<BuiltinLanguages, BuiltinThemes>
