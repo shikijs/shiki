0d2307b8ec94da6075a85dc83cab5b9eec34213b
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-31 17:26:05 +0200

    ci: ignore `/tmp/` in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testMatch: ['<rootDir>/packages/**/__tests__/**/*test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/tmp/'],
   testTimeout: 10000,
   testEnvironment: 'node',
   globals: {
