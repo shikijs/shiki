f3b26607ec65c287947c393dba3b3c8674fba9f1
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-14 19:20:12 +0100

    chore: fix test

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,12 +10,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      'shikiji': fileURLToPath(new URL('./packages/shikiji/src/index.ts', import.meta.url)),
       'shikiji/core': fileURLToPath(new URL('./packages/shikiji/src/core/index.ts', import.meta.url)),
       'shikiji-transformers': fileURLToPath(new URL('./packages/shikiji-transformers/src/index.ts', import.meta.url)),
       'shikiji-compat': fileURLToPath(new URL('./packages/shikiji-compat/src/index.ts', import.meta.url)),
       'shikiji-twoslash': fileURLToPath(new URL('./packages/shikiji-twoslash/src/index.ts', import.meta.url)),
       'markdown-it-shikiji': fileURLToPath(new URL('./packages/markdown-it-shikiji/src/index.ts', import.meta.url)),
+      'shikiji': fileURLToPath(new URL('./packages/shikiji/src/index.ts', import.meta.url)),
     },
   },
   test: {
