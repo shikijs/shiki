a38589eca65d1a2b29ce6e5608176e6fad451eba
morning-star <Chenguangstart@outlook.com>
2023-12-20 05:33:11 +0800

    docs: optimize selector style for mini playground (#55)

--- docs/.vitepress/components/ShikijiMiniPlayground.vue
+++ docs/.vitepress/components/ShikijiMiniPlayground.vue
@@ -10,13 +10,13 @@ const currentThemeType = computed(() => play.allThemes.find(i => i.id === play.t
   <div class="language-ts vp-adaptive-theme mini-playground" shadow :style="[play.preStyle, { colorScheme: currentThemeType }]">
     <div absolute z-10 p2 px3 pl5 flex="~ gap-1 items-center" left-0 top-0 right-0 border="b-solid gray/5">
       <div i-carbon:chevron-down op50 />
-      <select v-model="play.lang" font-mono>
+      <select v-model="play.lang" font-mono :style="play.preStyle">
         <option v-for="lang in play.allLanguages" :key="lang.id" :value="lang.id">
           {{ lang.name }}
         </option>
       </select>
       <div i-carbon:chevron-down op50 />
-      <select v-model="play.theme" font-mono>
+      <select v-model="play.theme" font-mono :style="play.preStyle">
         <option v-for="theme in play.allThemes.filter(i => i.type === 'light')" :key="theme.id" :value="theme.id">
           {{ theme.name }}
         </option>
