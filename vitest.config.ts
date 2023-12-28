0ccae7375baeca76533be8a39cf5b0c92d9fdd9b
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-28 21:10:43 +0100

    chore: fix coverage exclude

--- vitest.config.ts
+++ vitest.config.ts
@@ -26,7 +26,6 @@ export default defineConfig({
     exclude: [
       '**/vendor/**',
       '**/node_modules/**',
-      '**/shikiji-cli/**',
     ],
     server: {
       deps: {
@@ -48,6 +47,7 @@ export default defineConfig({
         '**/scripts/**',
         '**/vendor/**',
         '**/stack-element-metadata.ts',
+        '**/shikiji-cli/**',
         '**/shikiji-monaco/**',
       ],
     },
