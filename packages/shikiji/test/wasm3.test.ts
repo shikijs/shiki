e9df4b378b32da19ba564edae5727506e0708793
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-01 21:15:10 +0100

    chore: lint

--- packages/shikiji/test/wasm3.test.ts
+++ packages/shikiji/test/wasm3.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { expect, it } from 'vitest'
 import { getHighlighterCore } from '../src/core'
 
 import js from '../src/assets/langs/javascript'
