984040f01c5d8ef00e1abac42824c579b2947c2a
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-09 03:52:04 +0100

    docs: improve twoslash hover present

--- docs/.vitepress/renderer-floating-vue.ts
+++ docs/.vitepress/renderer-floating-vue.ts
@@ -2,29 +2,37 @@ import { rendererRich } from 'shikiji-twoslash'
 import type { TwoSlashRenderers } from 'shikiji-twoslash'
 import type { Element, Text } from 'hast'
 import type { ShikijiTransformerContext } from 'shikiji'
+import { gfmFromMarkdown } from 'mdast-util-gfm'
+import { fromMarkdown } from 'mdast-util-from-markdown'
+import { toHast } from 'mdast-util-to-hast'
 
 const rich = rendererRich({
   classExtra: 'vp-copy-ignore',
 })
 
 function createFloatingVueWarpper(this: ShikijiTransformerContext, text: string, docs: string | undefined, node: Element | Text, presisted = false): Element {
-  const themedContent = (this.codeToHast(text, {
-    ...this.options,
-    transformers: [],
-    transforms: undefined,
-  }).children[0] as Element).children
+  const themedContent = (this.codeToHast(
+    text
+      .replace(/^\(.+?\)\s+/, '')
+      .replace(/\nimport .*$/, '')
+      .replace(/\bshikiji_core\./g, '')
+      .trim()
+    ,
+    {
+      ...this.options,
+      transformers: [],
+      transforms: undefined,
+    },
+  ).children[0] as Element).children
 
   if (docs) {
     themedContent.push({
       type: 'element',
       tagName: 'div',
-      properties: { class: 'twoslash-popup-jsdoc' },
-      children: [
-        {
-          type: 'text',
-          value: docs,
-        },
-      ],
+      properties: { class: 'twoslash-popup-jsdoc vp-doc' },
+      children: (toHast(fromMarkdown(docs, {
+        mdastExtensions: [gfmFromMarkdown()],
+      })) as any).children,
     })
   }
 
