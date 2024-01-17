649c7e92aaa2969216f9dc525afae5ad566e879e
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-17 16:12:21 +0100

    fix(vitepress-twoslash): improve tabs experience

--- packages/vitepress-plugin-twoslash/src/client.ts
+++ packages/vitepress-plugin-twoslash/src/client.ts
@@ -1,5 +1,5 @@
 import type { Plugin } from 'vue'
-import FloatingVue from 'floating-vue'
+import FloatingVue, { recomputeAllPoppers } from 'floating-vue'
 
 const isMobile = typeof navigator !== 'undefined' && /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 
@@ -12,6 +12,15 @@ export type FloatingVueConfig = Parameters<(typeof FloatingVue)['install']>[1]
  */
 const TwoslashFloatingVue: Plugin<[FloatingVueConfig?]> = {
   install: (app, options: FloatingVueConfig = {}) => {
+    if (typeof window !== 'undefined') {
+      // Recompute poppers when clicking on a tab
+      window.addEventListener('click', (e) => {
+        const path = e.composedPath()
+        if (path.some((el: any) => el?.classList?.contains?.('vp-code-group') || el?.classList?.contains?.('tabs')))
+          recomputeAllPoppers()
+      }, { passive: true })
+    }
+
     app.use(FloatingVue, {
       ...options,
       themes: {
