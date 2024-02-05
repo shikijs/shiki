bd38e14ae3e90b4e7463db52370abad76b40db45
Anthony Fu <anthonyfu117@hotmail.com>
2024-02-05 16:40:22 +0100

    fix(vitepress): support custom explicitTrigger

--- packages/vitepress-twoslash/src/index.ts
+++ packages/vitepress-twoslash/src/index.ts
@@ -21,14 +21,22 @@ export interface VitePressPluginTwoslashOptions extends TransformerTwoslashOptio
  * Add this to `markdown.codeTransformers` in `.vitepress/config.ts`
  */
 export function transformerTwoslash(options: VitePressPluginTwoslashOptions = {}): ShikiTransformer {
+  const {
+    explicitTrigger = true,
+  } = options
+
   const twoslash = createTransformerFactory(
     createTwoslasher(),
   )({
     langs: ['ts', 'tsx', 'js', 'jsx', 'json', 'vue'],
-    explicitTrigger: true,
     renderer: rendererFloatingVue(options),
     ...options,
+    explicitTrigger,
   })
+
+  const trigger = explicitTrigger instanceof RegExp
+    ? explicitTrigger
+    : /\btwoslash\b/
 
   return {
     ...twoslash,
@@ -39,7 +47,7 @@ export function transformerTwoslash(options: VitePressPluginTwoslashOptions = {}
         options.transformers?.splice(options.transformers.indexOf(cleanup), 1)
 
       // Disable v-pre for twoslash, because we need render it with FloatingVue
-      if (options.meta?.__raw?.includes('twoslash')) {
+      if (!explicitTrigger || options.meta?.__raw?.match(trigger)) {
         const vPre = options.transformers?.find(i => i.name === 'vitepress:v-pre')
         if (vPre)
           options.transformers?.splice(options.transformers.indexOf(vPre), 1)
