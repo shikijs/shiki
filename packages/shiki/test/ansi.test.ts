ccb58331464ff25b25d7385be700a00edce1ad4e
Felix Koppe <hi@flxkpe.io>
2024-02-21 13:02:31 +0100

    fix: allow nullish color for colorReplacement, fix ansi highlighting with dark-plus, fix #597 (#598)

--- packages/shiki/test/ansi.test.ts
+++ packages/shiki/test/ansi.test.ts
@@ -31,3 +31,16 @@ Packages: [0;32m+1038[0m[0m
 
   expect(out).toMatchFileSnapshot('./out/ansi-background.html')
 })
+
+// https://github.com/shikijs/shiki/issues/597
+it('renders ansi to html with theme dark-plus', async () => {
+  const out = await codeToHtml(`[0;30;43m WARN [0m using --force I sure hope you know what you are doing
+Scope: all 6 workspace projects
+Lockfile is up to date, resolution step is skipped
+Packages: [0;32m+952[0m
+[0;32m++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[0m
+Progress: resolved [0;104m952[0m, reused [0;104m910[0m, downloaded [0;104m42[0m, added [0;104m952[0m, done
+Done in 15.7s`, { theme: 'dark-plus', lang: 'ansi' })
+
+  expect(out).toMatchFileSnapshot('./out/ansi-dark-plus.html')
+})
