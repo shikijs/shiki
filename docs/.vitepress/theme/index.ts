639aa6b0629a81c97e191f1aa2bc36edd833969b
Anthony Fu <anthonyfu117@hotmail.com>
2024-03-26 16:59:46 +0100

    docs: add link to v1 blog post

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import Theme from 'vitepress/theme'
 import type { EnhanceAppContext } from 'vitepress'
 import { createPinia } from 'pinia'
+import { h } from 'vue'
 import TwoslashFloatingVue from '../../../packages/vitepress-twoslash/src/client'
 
 import '../../../packages/twoslash/style-rich.css'
@@ -11,10 +12,23 @@ import 'uno.css'
 import './style.css'
 import './transformers.css'
 
+// @unocss-include
+
 export default {
   extends: Theme,
   enhanceApp({ app }: EnhanceAppContext) {
     app.use(createPinia())
     app.use(TwoslashFloatingVue)
+  },
+  Layout() {
+    return h(Theme.Layout, null, {
+      'home-hero-actions-after': () => h('div', { class: 'mt-10 mb--4 vp-doc' }, [
+        h('a', { href: 'https://nuxt.com/blog/shiki-v1', target: '_blank', class: 'no-underline! flex-inline gap-1 items-center' }, [
+          h('div', { class: 'i-ph-books-duotone text-2xl' }),
+          'The Evolution of Shiki v1.0',
+          h('div', { class: 'i-ph-arrow-up-right mt--3 ml--1' }),
+        ]),
+      ]),
+    })
   },
 }
