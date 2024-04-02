bb1fd333bdb992d8a12645be10a11076f1736a6c
bluwy <bjornlu.dev@gmail.com>
2024-04-02 23:25:57 +0800

    refactor: cache setTheme createFromRawTheme

--- packages/core/src/textmate.ts
+++ packages/core/src/textmate.ts
@@ -1,6 +1,7 @@
 // We re-bundled vscode-textmate from source to have ESM support.
 // This file re-exports some runtime values we need.
 export { Registry, INITIAL, StateStack } from '../vendor/vscode-textmate/src/main'
+export { Theme } from '../vendor/vscode-textmate/src/theme'
 export type {
   IRawTheme,
   IRawGrammar,
