7cf4bced9b5ee8530ad626c2f16f8c1ae0cabb2c
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-30 11:35:28 +0200

    chore: update snap

--- packages/shiki/test/bundle.test.ts
+++ packages/shiki/test/bundle.test.ts
@@ -7,7 +7,7 @@ it('bundle-full', async () => {
   }))
 
   expect(highlighter.getLoadedLanguages().length)
-    .toMatchInlineSnapshot(`260`)
+    .toMatchInlineSnapshot(`270`)
 })
 
 it('bundle-web', async () => {
