d1753863c02b2d3491750a36eb1f6200e54fee82
抬头看乌鸦啊 <ChandlerVer5@gmail.com>
2024-07-20 22:09:18 +0800

    fix: codeToTokens's includeExplanation option

--- packages/core/src/code-to-tokens-themes.ts
+++ packages/core/src/code-to-tokens-themes.ts
@@ -34,6 +34,10 @@ export function codeToTokensWithThemes(
           offset: _token.offset,
         }
 
+        if ('includeExplanation' in options) {
+          mergedToken.explanation = _token.explanation
+        }
+
         tokens.forEach((t, themeIdx) => {
           const {
             content: _,
