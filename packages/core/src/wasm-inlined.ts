3f2269e96f4ff0bb80fe9b4cea408b5e3852be25
Anthony Fu <github@antfu.me>
2024-09-13 16:26:15 +0200

    fix: missing named exports for `shiki/wasm` and `@shikijs/wasm-inlined`

--- packages/core/src/wasm-inlined.ts
+++ packages/core/src/wasm-inlined.ts
@@ -1,1 +1,2 @@
 export { default } from '@shikijs/engine-oniguruma/wasm-inlined'
+export * from '@shikijs/engine-oniguruma/wasm-inlined'
--- packages/shiki/src/wasm.ts
+++ packages/shiki/src/wasm.ts
@@ -1,1 +1,2 @@
 export { default } from '@shikijs/core/wasm-inlined'
+export * from '@shikijs/core/wasm-inlined'
