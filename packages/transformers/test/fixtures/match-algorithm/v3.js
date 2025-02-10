8bfd8c5d1dced05522b1ef96413be02d3af8f758
Fro-Q <froqqq@outlook.com>
2025-02-10 19:36:49 +0800

    test(transformers): add test cases in both v1 an v3 (#921)

--- packages/transformers/test/fixtures/match-algorithm/v3.js
+++ packages/transformers/test/fixtures/match-algorithm/v3.js
@@ -20,3 +20,20 @@ let options = 'options'
 options = {}, // [!code word:log]
 console.log(options)
 options.a = "HELLO" // should not be highlighted
+
+// [!code focus:3-5]
+console.log('not focus')
+console.log('not focus')
+console.log('focus')
+console.log('focus')
+console.log('focus')
+
+// [!code highlight:2-3]
+console.log('not highlighted')
+console.log('highlighted')
+console.log('highlighted')
+
+console.log('not highlighted') // [!code highlight:3-4]
+console.log('not highlighted')
+console.log('highlighted')
+console.log('highlighted')
