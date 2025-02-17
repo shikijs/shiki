d619571dde3c173457dbbccb10573d8ab1877d39
Anthony Fu <github@antfu.me>
2025-02-17 11:54:56 +0800

    fix: missing types for `CodeToTokensWithThemesOptions`

--- packages/types/src/tokens.ts
+++ packages/types/src/tokens.ts
@@ -31,7 +31,7 @@ export interface CodeToTokensBaseOptions<Languages extends string = string, Them
 export type CodeToTokensOptions<Languages extends string = string, Themes extends string = string> = Omit<CodeToTokensBaseOptions<Languages, Themes>, 'theme'> &
   CodeOptionsThemes<Themes>
 
-export interface CodeToTokensWithThemesOptions<Languages = string, Themes = string> {
+export interface CodeToTokensWithThemesOptions<Languages = string, Themes = string> extends TokenizeWithThemeOptions {
   lang?: Languages | SpecialLanguage
 
   /**
