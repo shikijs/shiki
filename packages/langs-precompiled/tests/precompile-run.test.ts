7faa92a5c17ac370c7e44b915a721b309930560c
Anthony Fu <github@antfu.me>
2025-02-17 15:08:06 +0800

    chore: update test

--- packages/langs-precompiled/tests/precompile-run.test.ts
+++ packages/langs-precompiled/tests/precompile-run.test.ts
@@ -3,15 +3,13 @@ import { createHighlighterCore } from 'shiki'
 import { expect, it } from 'vitest'
 // eslint-disable-next-line antfu/no-import-dist
 import vitesseDark from '../../themes/dist/vitesse-dark.mjs'
-// eslint-disable-next-line antfu/no-import-dist
-import html from '../dist/html.mjs'
 
 const isNode20andUp = process.version.replace(/^v/, '').split('.').map(Number)[0] >= 20
 
 it.runIf(isNode20andUp)('run', async () => {
   const shiki = await createHighlighterCore({
     themes: [vitesseDark],
-    langs: [html],
+    langs: [import('../dist/html.mjs')],
     engine: createJavaScriptRawEngine(),
   })
 
