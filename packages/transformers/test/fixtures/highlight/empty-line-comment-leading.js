5546c148048c9236384488b85d82e5f96933c798
Anthony Fu <github@antfu.me>
2025-02-18 15:22:45 +0800

    chore: add more fixture for transformer

---
+++ packages/transformers/test/fixtures/highlight/empty-line-comment-leading.js
@@ -0,0 +1,9 @@
+// [!code highlight:4]
+export function transformerNotationFocus(
+  options = {},
+) {
+  const {
+    classFocused = 'focused',
+    classActivePre = 'has-focused',
+  } = options
+}
