2e637f73db3b37b2e31ef38988df74c00d0de166
zhangmo8 <wegi866@gmail.com>
2025-04-18 15:44:16 +0800

    docs: delete the min-height of the home page demo

--- docs/.vitepress/components/HomeDemo.vue
+++ docs/.vitepress/components/HomeDemo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container max-w-250 mx-a my-10 min-h-250 vp-doc p4" of-hidden>
+  <div class="container max-w-250 mx-a my-10 vp-doc p4" of-hidden>
     <ShikiMiniPlayground />
   </div>
 </template>
