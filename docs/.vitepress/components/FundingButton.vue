85e69f92ca821e07d5d3b549ac2445e48e9da01b
Anthony Fu <github@antfu.me>
2025-08-04 16:29:58 +0900

    chore: update

--- docs/.vitepress/components/FundingButton.vue
+++ docs/.vitepress/components/FundingButton.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { FundingLink } from 'tm-grammars'
+import { Tooltip } from 'floating-vue'
 
 defineProps<{
   funding: FundingLink[] | undefined
@@ -8,10 +9,11 @@ defineProps<{
 </script>
 
 <template>
-  <div
+  <Tooltip
     v-if="funding && funding.length > 0"
+    theme="twoslash"
     class="group"
-    relative inline-block
+    relative inline-block mya align-middle
   >
     <button
       title="Funding"
@@ -19,35 +21,26 @@ defineProps<{
       p1 rounded
     >
       <div
-        i-carbon:favorite-filled text="red-500"
-        op50 group-hover:op100 group-focus-within:op100
-        transition-opacity duration-250 class="ease-[step-end] group-hover:ease-[step-start] group-focus-within:ease-[step-start]"
+        i-carbon:favorite-filled
+        op25 group-hover:op100 group-focus-within:op100 group-hover:text-red-500
+        transition-opacity duration-250
       />
     </button>
 
-    <div
-      class="ease-[step-end] group-hover:ease-[step-start] group-focus-within:ease-[step-start] transition-[opacity,visibility]"
-      absolute top-full left-0 z-1
-      bg-white border="~ solid gray-200"
-      p3 text="xs"
-      rounded shadow-xl
-      opacity-0 invisible
-      group-hover:opacity-100 group-hover:visible
-      group-focus-within:opacity-100 group-focus-within:visible
-      duration-250
-      text-nowrap
-    >
-      <strong block mb-2>Support {{ name }} development:</strong>
-      <div
-        v-for="link, i in funding"
-        :key="i"
-        text-nowrap
-      >
-        <template v-if="link.handle">
-          {{ link.name }}:
-        </template>
-        <a :href="link.url" target="_blank">{{ link.handle || link.name }}</a>
+    <template #popper>
+      <div p2 class="vp-doc text-sm">
+        <strong block mb-2>Support {{ name }} development:</strong>
+        <div
+          v-for="link, i in funding"
+          :key="i"
+          text-nowrap
+        >
+          <template v-if="link.handle">
+            {{ link.name }}:
+          </template>
+          <a :href="link.url" target="_blank">{{ link.handle || link.name }}</a>
+        </div>
       </div>
-    </div>
-  </div>
+    </template>
+  </Tooltip>
 </template>
