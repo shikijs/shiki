3f8ab7e109be55bcb673c9e06823e544004e9e06
KazariEX <zinkawaii@qq.com>
2025-08-13 23:05:36 +0800

    feat(transformers): render indent guides

---
+++ packages/transformers/src/transformers/render-indent-guides.ts
@@ -0,0 +1,82 @@
+import type { Element } from 'hast'
+import type { ShikiTransformer } from 'shiki'
+
+export interface TransformerRenderIndentGuidesOptions {
+  indent?: number
+}
+
+/**
+ * Render indents as separate tokens.
+ * Apply with CSS, it can be used to render indent guides visually.
+ */
+export function transformerRenderIndentGuides(
+  options: TransformerRenderIndentGuidesOptions = {},
+): ShikiTransformer {
+  const {
+    indent = 2,
+  } = options
+
+  const indentRegex = new RegExp(` {${indent}}| {0,${indent - 1}}\t| {1,}$`, 'g')
+
+  return {
+    name: '@shikijs/transformers:render-indent-guides',
+    code(hast) {
+      const emptyLines: [Element, number][] = []
+      let level = 0
+
+      for (const line of hast.children) {
+        if (line.type !== 'element') {
+          continue
+        }
+
+        const first = line.children[0]
+        if (first?.type !== 'element' || first?.children[0]?.type !== 'text') {
+          emptyLines.push([line, level])
+          continue
+        }
+
+        const text = first.children[0]
+        const leading = text.value.split(/[^ \t]/, 1)[0]
+
+        const ranges: [number, number][] = []
+        for (const match of leading.matchAll(indentRegex)) {
+          const start = match.index
+          const end = start + match[0].length
+          ranges.push([start, end])
+        }
+
+        for (const [line, level] of emptyLines) {
+          line.children.unshift(...Array.from({ length: Math.min(ranges.length, level + 1) }, (_, i) => ({
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              class: 'indent',
+              style: `--indent-left: ${i * indent}ch;`,
+            },
+            children: [],
+          } satisfies Element)))
+        }
+        emptyLines.length = 0
+        level = ranges.length
+
+        if (ranges.length) {
+          line.children.unshift(
+            ...ranges.map(([start, end]) => ({
+              type: 'element',
+              tagName: 'span',
+              properties: {
+                class: 'indent',
+              },
+              children: [{
+                type: 'text',
+                value: text.value.slice(start, end),
+              }],
+            } satisfies Element)),
+          )
+          text.value = text.value.slice(ranges.at(-1)![1])
+        }
+      }
+      return hast
+    },
+  }
+}
