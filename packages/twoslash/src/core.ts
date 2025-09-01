ffd5c9ed301335f48a1276bf4765a1f89c979db0
Karibash <y.karibayashi@gmail.com>
2025-09-01 11:30:07 +0900

    fix(twoslash): Make it so that the code is replaced with the return value of onTwoslashError

--- packages/twoslash/src/core.ts
+++ packages/twoslash/src/core.ts
@@ -129,7 +129,7 @@ export function createTransformerFactory(
           catch (error) {
             const result = onTwoslashError(error, code, lang, this.options)
             if (typeof result === 'string')
-              return code
+              return result
           }
         }
       },
