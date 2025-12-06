3b4bf0176004ad206a018c03c82e6b42f4d76458
Khyati-Kapil <khyatekapil@gmail.com>
2025-12-06 22:54:06 +0530

    fix: add plaintext + markdown diff marker support ([!code ++], [!code --]) \

--- packages/core/src/highlight/_get-transformers.ts
+++ packages/core/src/highlight/_get-transformers.ts
@@ -16,6 +16,7 @@ export function getTransformers(options: TransformerOptions): ShikiTransformer[]
   ]
 }
 
+
 function sortTransformersByEnforcement(transformers: ShikiTransformer[]): {
   pre: ShikiTransformer[]
   post: ShikiTransformer[]
