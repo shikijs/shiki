e2acdc4518efb40491a549ee4f591e8971e4f677
Anthony Fu <github@antfu.me>
2026-01-31 08:06:01 +0900

    chore: update snapshot

--- packages/shiki/test/bundle.test.ts
+++ packages/shiki/test/bundle.test.ts
@@ -7,7 +7,7 @@ it('bundle-full', async () => {
   }))
 
   expect(highlighter.getLoadedLanguages().length)
-    .toMatchInlineSnapshot(`339`)
+    .toMatchInlineSnapshot(`346`)
 })
 
 it('bundle-web', async () => {
